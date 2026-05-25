2d9ffe9c1d8e0be8bc49d839dd4203c4326044eb
Thomas Gonzalez <thomasgonzalezaguilar@gmail.com>
2026-05-25 10:40:51 -0500

    mellos

--- Program.cs
+++ Program.cs
@@ -1,25 +1,20 @@
 using FinalBackendWeb.DAO;
+using FinalBackendWeb.Interface;
 using FinalBackendWeb.Interfaces;
 using FinalBackendWeb.Services;
 using Microsoft.EntityFrameworkCore;
-using FinalBackendWeb.DAO;
-using FinalBackendWeb.Interfaces;
-using FinalBackendWeb.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuración de SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Inyección de Dependencias
-builder.Services.AddScoped<IPreguntaService, PreguntaService>();
-builder.Services.AddScoped<IRespuestaService, RespuestaService>();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -31,5 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
